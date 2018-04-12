e1b200744b1f51d89c01b1d411cceab573aa1edb
Alex Ferreira <arf92@live.com>
2018-04-12 07:38:36 -0300

    Finish the exercise, but without the impl of actions

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -3,6 +3,9 @@ package com.example.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -29,6 +32,29 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    // (3) Create a menu with an item with id of action_share
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+
+        return true;
+    }
+
+    // (4) Display the menu and implement the forecast sharing functionality
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_share_weather:
+                shareWeather();
+                break;
+        }
+
+        return true;
+    }
+
+    private void shareWeather() {
+        // todo share text
+    }
 }
--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -221,8 +221,15 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
             return true;
         }
 
-        // TODO (2) Launch the map when the map menu item is clicked
+        // (2) Launch the map when the map menu item is clicked
+        if (id == R.id.action_share_location) {
+            shareLocation();
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void shareLocation() {
+        // todo open map
     }
 }
